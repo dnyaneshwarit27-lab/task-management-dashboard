1ecfed7578894394ce68d6d901ea7509ea2a5e97
Dnyaneshwari Ashruba Thorat <dnyaneshwarit27@gmail.com>
2026-05-23 17:49:44 +0530

    Add files via upload

---
+++ backend/routes/taskRoutes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const {
+  getTasks,
+  getTaskById,
+  createTask,
+  updateTask,
+  deleteTask,
+} = require('../controllers/taskController');
+const { protect } = require('../middleware/authMiddleware');
+
+// Protect all routes
+router.use(protect);
+
+router.route('/')
+  .get(getTasks)
+  .post(createTask);
+
+router.route('/:id')
+  .get(getTaskById)
+  .put(updateTask)
+  .delete(deleteTask);
+
+module.exports = router;
